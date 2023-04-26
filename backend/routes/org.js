e6809a7dffb7fca9e1b415e8ded4abd24a9da688
smothman <89706436+smothman@users.noreply.github.com>
2023-04-26 10:21:22 +0500

    Update org.js

--- backend/routes/org.js
+++ backend/routes/org.js
@@ -17,4 +17,30 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.get('/getorg', async (req, res, next) => {
+  try {
+    // Find all documents in the 'org' collection
+    const allOrgs = await orgs.find({});
+    res.json(allOrgs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+// Route handler for POST request
+// POST new event
+router.post('/', (req, res, next) => {
+  const newEvent = req.body
+  newEvent.org = org
+  orgs.create(newEvent, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
 module.exports = router
